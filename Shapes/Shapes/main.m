f58ac9734f96eb818af2cb8e32f8f4a44cf5ff3d
NIXIN <Zadvornov.Aleksandr@gmail.com>
2011-06-02 23:31:19 +0400

    Создан пустой проект

---
+++ Shapes/Shapes/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Shapes
+//
+//  Created by Aleksandr on 02.06.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+    return 0;
+}
+
